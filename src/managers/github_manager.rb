55c894adf385e6007538a16f7a5a08900308eb0f
Nikolay Karagyozov <newmast@abv.bg>
2016-01-11 19:43:55 +0200

    fixed cloning

--- src/managers/github_manager.rb
+++ src/managers/github_manager.rb
@@ -2,6 +2,6 @@ require_relative './source_control_manager'
 
 class GitHubManager < SourceControlManager
   def download_repository(path, link)
-    `git clone #{link} #{path}`
+    `#{path} && git clone #{link}`
   end
 end
