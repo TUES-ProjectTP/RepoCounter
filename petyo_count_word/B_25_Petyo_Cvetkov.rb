172454090af1907c4c009bd16cfe44ad55c8ad0c
petiocvetkov <petiopetio@mail.bg>
2016-01-11 19:44:27 +0200

    asd

---
+++ petyo_count_word/B_25_Petyo_Cvetkov.rb
@@ -0,0 +1,27 @@
+require './B_25_Petyo_Cvetkov/CountWords.rb'
+
+path = ARGV[0]
+format = ARGV[1]
+
+if path == "-d"
+  result = WordCounter::parse_dir format
+else
+  if (path.match('^https?:\/\/.+'))
+    result = WordCounter::parse_webpage(path)
+  else
+    result = WordCounter::parse_file(path)
+  end
+end
+
+  WordCounter::Draw_graph::draw_graph(result.word_counts,result.marks_count)
+
+
+if format == 'json'
+  File.open('result.json', 'w') { |file| file << result.to_json }
+  puts result.to_json
+elsif format == 'xml'
+  File.open('result.xml', 'w') { |file| file << result.to_xml }
+  puts result.to_xml
+else
+  File.open("#{ARGV[2]}.csv", 'w') { |file| file << result.to_csv }
+end
---
+++ petyo_count_word/B_25_Petyo_Cvetkov/count_words/Count_words.rb
@@ -0,0 +1,50 @@
+module WordCounter
+  class Parser
+    def initialize
+      @text = " "
+    end
+    def parse(string)
+      result = Result.new
+      result.marks_count = string.scan(/[,?!@#$%^&*'""'()=+-]/).count
+      words = string.downcase.gsub(/[^a-z0-9\s-]/, ' ').split(' ')
+
+      words.each do |word|
+        result.word_counts[word] += 1
+      end
+
+      result.word_counts = result.word_counts.sort_by { |word, occur| [-occur, word] }
+      result
+    end
+
+
+
+    def parse_file(filename)
+      text = ''
+
+      File.open(filename) do |file|
+        file.each_line do |line|
+          text += line
+        end
+      end
+
+      parse text
+    end
+    def dir_parser(dir)
+      if dir[dir.size - 1] != "/"
+        dir += "/"
+      end
+      files = Dir.glob(dir + '*')
+      files.each do |filename|
+        if File.directory? filename
+          dir_parser filename
+        else
+        if File.extname(filename) == ".#{ARGV[2]}"
+        @text += File.read(filename)
+        @text += " "
+        end
+        end
+      end
+      parse(@text)
+    end
+  end
+end
